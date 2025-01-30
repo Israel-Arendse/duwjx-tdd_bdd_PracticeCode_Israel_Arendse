a5a798d7d53ee05f1f970a25166764b80fa2450b
Israel-Arendse <israelarendsefortnite@gmail.com>
2025-01-30 08:12:26 -0500

    Add 'test_delete_an_account' testcase to 04_test_coverage/tests/test_account.py

--- labs/04_test_coverage/tests/test_account.py
+++ labs/04_test_coverage/tests/test_account.py
@@ -97,3 +97,12 @@ class TestAccountModel(TestCase):
         account = Account(**data)
         account.id = None
         self.assertRaises(DataValidationError, account.update)
+
+    def test_delete_an_account(self):
+        """ Test account delete """
+        data = ACCOUNT_DATA[self.rand] # get a random account
+        account = Account(**data)
+        account.create()
+        self.assertEqual(len(Account.all()), 1)
+        account.delete()
+        self.assertEqual(len(Account.all()), 0)
