ad0e35888db8e135b0223486989190aeee9b0b59
Israel-Arendse <israelarendsefortnite@gmail.com>
2025-02-03 08:13:34 -0500

    Fixed typo for 'models.imdb.requests.get' in test_imdb.py

--- labs/06_mocking_objects/tests/test_imdb.py
+++ labs/06_mocking_objects/tests/test_imdb.py
@@ -38,8 +38,8 @@ class TestIMDbDatabase(TestCase):
         self.assertEqual(results["results"][0]["id"], "tt1375666")
 
     # Testcase for search_with_no_results
-    # Mock: 404 status code f
-    @patch('models.imdb.request.get')
+    # Mock: 404 status code
+    @patch('models.imdb.requests.get')
     def test_search_with_no_results(self, imdb_mock):
         """Test searching with no results"""
         imdb_mock.return_value = Mock(status_code=404)
