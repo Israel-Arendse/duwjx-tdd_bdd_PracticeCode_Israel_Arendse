e8ab178de3831c0b49e29ddc4787e188853d02fd
Israel-Arendse <israelarendsefortnite@gmail.com>
2025-02-09 06:53:48 -0500

    Implemented step defintions in web_steps.py

--- labs/11_generating_steps/features/steps/web_steps.py
+++ labs/11_generating_steps/features/steps/web_steps.py
@@ -10,3 +10,39 @@ For information on Waiting until elements are present in the HTML see:
 # Import from behave and selenium
 from  behave import given, when, then
 from selenium.webdriver.common.by import By
+
+# step definitions
+
+@given(u'I am on the "Home Page"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given I am on the "Home Page"')
+
+
+@when(u'I set the "Category" to "dog"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I set the "Category" to "dog"')
+
+
+@when(u'I click the "Search" button')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I click the "Search" button')
+
+
+@then(u'I should see the message "Success"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should see the message "Success"')
+
+
+@then(u'I should see "Fido" in the results')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should see "Fido" in the results')
+
+
+@then(u'I should not see "Kitty" in the results')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should not see "Kitty" in the results')
+
+
+@then(u'I should not see "Leo" in the results')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should not see "Leo" in the results')
