f581840522bd8c6df0c1988c16eda9a8a73cc197
Israel-Arendse <israelarendsefortnite@gmail.com>
2025-02-09 06:47:59 -0500

    Add imports to websteps.py

--- labs/11_generating_steps/features/steps/web_steps.py
+++ labs/11_generating_steps/features/steps/web_steps.py
@@ -7,4 +7,6 @@ For information on Waiting until elements are present in the HTML see:
     https://selenium-python.readthedocs.io/waits.html
 """
 
-# Replace this comment with your web steps...
+# Import from behave and selenium
+from  behave import given, when, then
+from selenium.webdriver.common.by import By
